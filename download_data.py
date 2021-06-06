ad01e03b5f5c11f1a99c54e531b7661dee37ade5
nicoloc <nico.campolongo@gmail.com>
2021-06-06 22:03:01 +0200

    adding comments

--- download_data.py
+++ download_data.py
@@ -3,87 +3,97 @@ from pathlib import Path
 import pandas as pd
 from ast import literal_eval
 from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, accuracy_score
-import joblib
+from script import classifier
 
 
-def preprocess(movies_data_path):
-    """
-    Function to preprocess the movies dataset.
-    Largely inspired to https://www.kaggle.com/neerajkhadagade/predicting-the-movie-genres.
-    """
+class kaggleDataset(object):
 
-    # read the csv
-    df = pd.read_csv(movies_data_path / 'movies_metadata.csv')
-    # drop unnecessary columns
-    df = df[['title', 'tagline', 'original_title', 'overview', 'genres']]
-    df.set_index('title',inplace = True)
-    df.dropna(subset=['overview'], inplace=True)
-    df['genres'] = df['genres'].apply(literal_eval).apply(
-        lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+    def __init__(self, storing_path):
+        self.data_path = storing_path
     
-    # Selecting only those rows which have an actual genre
-    genre_present = df['genres'] != '[]'
-    # Series of the genres present in the movies_metadata
-    genres = df['genres'][genre_present]
+    def download_and_extract(self):
+        """Download the movies dataset https://www.kaggle.com/rounakbanik/the-movies-dataset from Kaggle.
+        """
+        # create data directory
+        self.data_path.mkdir(parents=True, exist_ok=True)
+        movies_metadata = self.data_path / "movies_metadata.csv"
 
-    # Step 2: Separating and selecting the genres
-    mlb = MultiLabelBinarizer()
-    labels = mlb.fit_transform(genres)
-    label_classes = mlb.classes_
-    label_data = pd.DataFrame(labels, columns=label_classes)
-    selected_genres = label_data.sum(axis=0)
-    selected_genres = selected_genres[selected_genres > 1]
-    final_genres = MultiLabelBinarizer(classes=selected_genres.index)
-    y = final_genres.fit_transform(genres)
-    # Including only the rows with labels present
-    no_label_classes = y.sum(axis = 1) == 0
-    y = y[~no_label_classes]
-    
-    # STEP 3: create feature vectors
-    X = df['overview'][~no_label_classes]
-    X_train, X_valid, y_train, y_valid = train_test_split(
-        X, y, test_size = 0.3, random_state = 42)
+        # download data if not present
+        if not movies_metadata.exists():
+            # use kaggle api to download data
+            kaggle.api.authenticate()
+            kaggle.api.dataset_download_files('rounakbanik/the-movies-dataset', path=self.data_path, unzip=True)
 
-    vectorizer = TfidfVectorizer(max_features = 1000, stop_words='english', lowercase=True)
-    X_train_vec = vectorizer.fit_transform(X_train)
-    X_valid_vec = vectorizer.transform(X_valid)
-    return X_train_vec, X_valid_vec, y_train, y_valid, [i for i in selected_genres.index], vectorizer
+            # remove unnecessary files
+            unused_files = [i for i in self.data_path.iterdir() if i != self.data_path / "movies_metadata.csv"]
+            for f in unused_files:
+                f.unlink()
 
-                                                                
+        # update data path
+        self.data_path = self.data_path / "movies_metadata.csv"
+
+    def preprocess(self):
+        """
+        Function to preprocess the movies dataset.
+        Largely inspired to https://www.kaggle.com/neerajkhadagade/predicting-the-movie-genres.
+        """
+        # read the csv
+        df = pd.read_csv(self.data_path)
+
+        # drop unnecessary columns
+        df = df[['title', 'overview', 'genres']]
+        df.set_index('title',inplace = True)
+        df.dropna(subset=['overview'], inplace=True)
+        df['genres'] = df['genres'].apply(literal_eval).apply(
+            lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+        
+        # Selecting only those rows which have an actual genre
+        genre_present = df['genres'] != '[]'
+        # Series of the genres present in the movies_metadata
+        genres = df['genres'][genre_present]
+
+        # Step 2: Separating and selecting the genres
+        mlb = MultiLabelBinarizer()
+        labels = mlb.fit_transform(genres)
+        label_classes = mlb.classes_
+        label_data = pd.DataFrame(labels, columns=label_classes)
+        selected_genres = label_data.sum(axis=0)
+        selected_genres = selected_genres[selected_genres > 1].index
+        final_genres = MultiLabelBinarizer(classes=selected_genres)
+        y = final_genres.fit_transform(genres)
+
+        # Including only the rows with labels present
+        no_label_classes = y.sum(axis = 1) == 0
+        y = y[~no_label_classes]
+        X = df['overview'][~no_label_classes]
+        return X, y, list(selected_genres)
+
+             
 if __name__ == "__main__":
-    data_path = Path("./data")
-    # data_path.mkdir(parents=True, exist_ok=True)
-    # kaggle.api.authenticate()
-    # kaggle.api.dataset_download_files('rounakbanik/the-movies-dataset', path=data_path, unzip=True)
-    # unused_files = [i for i in data_path.iterdir() if i != data_path / "movies_metadata.csv"]
-    # for f in unused_files:
-    #     f.unlink()
-    X1, X2, y1, y2, genre_counts, vect = preprocess(data_path)
-
-    ### Random Forest Classifier
-    model = RandomForestClassifier()
-    model.fit(X1, y1)
-    train_pred = model.predict(X1)
-    valid_pred = model.predict(X2)
-
-    print("Classification Report")
-    print("Training:\n", classification_report(y_true=y1, y_pred=train_pred, target_names=genre_counts))
-    print("Validation:\n", classification_report(y_true=y2, y_pred=valid_pred, target_names=genre_counts))
-
-    print("Accuracy")
-    train_rfc_acc = accuracy_score(y_true=y1, y_pred=train_pred)
-    valid_rfc_acc = accuracy_score(y_true=y2, y_pred=valid_pred)
-    print("Traning: ", train_rfc_acc)
-    print("Validation: ",valid_rfc_acc)
-
-    # save
-    joblib.dump(model, data_path / "model.pkl")
-    joblib.dump(vect, data_path / "vect.pkl")
+    storing_path = Path("./data")
+    dataset = kaggleDataset(storing_path)
+    dataset.download_and_extract()
+    X, Y, genres = dataset.preprocess()
+    rf_classifier = classifier(storing_path)
+    rf_classifier.train(X, Y, genres)
 
 
+    # ### Random Forest Classifier
+    # model = RandomForestClassifier()
+    # model.fit(X1, y1)
+    # train_pred = model.predict(X1)
+    # valid_pred = model.predict(X2)
 
+    # print("Classification Report")
+    # print("Training:\n", classification_report(y_true=y1, y_pred=train_pred, target_names=genre_counts))
+    # print("Validation:\n", classification_report(y_true=y2, y_pred=valid_pred, target_names=genre_counts))
+
+    # print("Accuracy")
+    # train_rfc_acc = accuracy_score(y_true=y1, y_pred=train_pred)
+    # valid_rfc_acc = accuracy_score(y_true=y2, y_pred=valid_pred)
+    # print("Traning: ", train_rfc_acc)
+    # print("Validation: ",valid_rfc_acc)
+
+    # # save
+    # joblib.dump(model, data_path / "model.pkl")
+    # joblib.dump(vect, data_path / "vect.pkl")
--- script.py
+++ script.py
@@ -6,11 +6,15 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.ensemble import RandomForestClassifier
+from numpy import where
 
 
 class classifier(object):
 
     def __init__(self, data_path: Path, max_features: int=1000, test_size: float=.3, verbose: bool=True):
+        """
+        TODO: Explain arguments.
+        """
         if max_features < 1:
             raise ValueError("Please specify a value greater than 0 for max_features!")
         if test_size < 0 or test_size > 1:
@@ -28,71 +32,115 @@ class classifier(object):
         """
         if self.verbose:
             print("Loading model..")
-        self.model = joblib.load(self.data_path / "model.pkl")
-        self.vect = joblib.load(self.data_path / "vect.pkl")
+        model_dict = joblib.load(self.data_path / "model.pkl")
+        self.model = model_dict["model"]
+        self.vect = model_dict["vect"]
+        self.genres = model_dict["genres"]
         return
+
+    def set_genres(self, x):
+        self.genres = x
 
     def train(self, features, targets, genres, save: bool=True):
         """Train a Random Forest Classifier.
+        Arguments:
+            features: Pandas series where each value is a string
+                representing the description of the corresponding movie
+                which will be used for training.
+            targets: Pandas DataFrame where row i is a 0/1 vector
+                representing the presence of the corresponding label.
+            genres: list of strings representing the genres.
         """
-        # Todo: adding checks for features and targets
+        if len(features) != len(targets):
+            raise ValueError("Length of features and targets differ!")
+        if len(features) < 2:
+            raise RuntimeError("Please consider using a bigger training set!")
+        if len(genres) == 0:
+            raise RuntimeError("No genres provided!") 
 
         # initialize the random forest and vectorizer
         self.model = RandomForestClassifier()
-        self.vectorizer = TfidfVectorizer(max_features = 1000, stop_words='english', lowercase=True)
+        self.vect = TfidfVectorizer(
+            max_features=self.max_features, stop_words='english', lowercase=True)
+        self.genres = genres
 
         # split dataset
         X_train, X_valid, y_train, y_valid = train_test_split(
             features, targets, test_size=self.test_size, random_state=42)
 
         # transform descriptions into arrays
-        X_train_vec = self.vectorizer.fit_transform(X_train)
-        X_valid_vec = self.vectorizer.transform(X_valid)
+        X_train_vec = self.vect.fit_transform(X_train)
+        X_valid_vec = self.vect.transform(X_valid)
 
         # fit the model
+        if self.verbose:
+            print("Training the model...")
         self.model.fit(X_train_vec, y_train)
 
-        # display results and training and validation set
+        # save the model
+        if save:
+            if self.verbose:
+                print("Saving the model...")
+            model_dict = dict()
+            model_dict["model"] = self.model
+            model_dict["vect"] = self.vect
+            model_dict["genres"] = self.genres
+            joblib.dump(model_dict, self.data_path / "model.pkl")
+        if self.verbose:
+            print("Done!")
+
+        # display results on training and validation set
         if self.verbose:
             train_pred = self.model.predict(X_train_vec)
             valid_pred = self.model.predict(X_valid_vec)
 
             # print results
             print("Classification Report")
-            print("Training:\n", classification_report(y_true=y_train, y_pred=train_pred, target_names=genres))
-            print("Validation:\n", classification_report(y_true=y_valid, y_pred=valid_pred, target_names=genres))
+            print("Training:\n", classification_report(
+                y_true=y_train, y_pred=train_pred, target_names=self.genres))
+            print("Validation:\n", classification_report(
+                y_true=y_valid, y_pred=valid_pred, target_names=self.genres))
             print("Accuracy")
             train_acc = accuracy_score(y_true=y_train, y_pred=train_pred)
             valid_acc = accuracy_score(y_true=y_valid, y_pred=valid_pred)
             print("Traning: ", train_acc)
-            print("Validation: ",valid_acc)
+            print("Validation: ", valid_acc)
 
-        # save
-        if save:
-            joblib.dump(self.model, self.data_path / "model.pkl")
-            joblib.dump(self.vect, self.data_path / "vect.pkl")
         return
 
     def predict(self, title: str, description: str) -> Mapping[str, str]:
         """Predict movie genre based on description.
+        Arguments:
+            title: 
         """
-        assert type(title) == type(description) == str, "Please provide title and description as strings"
+        if type(title) != str:
+            raise TypeError("Please provide title as string.")
+        if type(description) != str:
+            raise TypeError("Please provide description as string.")
         if len(title) == 0:
             raise ValueError("Please provide a title!")
         if len(description) == 0:
             raise ValueError("Please provide a description!")
-        # todo: raise Error is model or vectorizer = None
+        if not self.model or not self.vect:
+            raise RuntimeError("Model not trained!")
         if self.verbose:
             print("We're working for you...")
+
+        # transform description into array
         feat_vec = self.vect.transform([description])
-        genre = self.model.predict(feat_vec)
-        result = {"title": title, "description": description, "genre": genre}
+
+        # generate prediction
+        pred = self.model.predict(feat_vec)
+        print(pred)
+        print(self.genres)
+        genre_ind = where(pred[0] == 1)[0][0]
+        result = {"title": title, "description": description, "genre": self.genres[genre_ind]}
         return result
 
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='This script is used to classify movies based on a title and description provided as inputs.')
     parser.add_argument('--title', default='', type=str, help='The title of the movie')
     parser.add_argument('--description', default='', type=str, help='The description of the movie')
     args = parser.parse_args()
