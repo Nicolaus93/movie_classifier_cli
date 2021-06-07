c704b9f7356fb71470001c9249b6aa640682352e
nicoloc <nico.campolongo@gmail.com>
2021-06-07 08:25:24 +0200

    added docstring

--- movie_classifier/__init__.py
+++ movie_classifier/__init__.py
@@ -1,4 +1,8 @@
-
+"""
+`movie_classifier` is a package implementing
+a command line interface used to classify
+movies based on title and description.
+"""
 from .cli import CLInterface
 from .dataset import KaggleDataset  
 from .random_forest import RandomForest  
