73959141e6b123bc8f3ef2ce196c10e0462ff421
nicoloc <nico.campolongo@gmail.com>
2021-06-09 10:04:10 +0200

    formatted

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     required_packages = f.read().splitlines()
 
 setup(
-    name='movie_classifier',
-    packages=['movie_classifier'],
-    version='0.0.1',
-    entry_points={
-        'console_scripts': ['movie_classifier=movie_classifier.cli:main']
-    },
-    install_requires=required_packages
+    name="movie_classifier",
+    packages=["movie_classifier"],
+    version="0.0.1",
+    entry_points={"console_scripts": ["movie_classifier=movie_classifier.cli:main"]},
+    install_requires=required_packages,
 )
